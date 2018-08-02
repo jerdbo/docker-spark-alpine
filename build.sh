397384e1445c5ef9c5b9ba529f1c3f16d63f2a4c
jiang <jiang.guo@pegasus.ae>
2018-08-02 14:47:02 +0800

    use alpine

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 set -e
 
 TAG=2.3.1-hadoop2.7
+server=alpine
 
 build() {
     NAME=$1
-    IMAGE=jerdbo/spark-$NAME:$TAG
+    IMAGE=jerdbo/$server-spark-$NAME:$TAG
     cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
     echo '--------------------------' building $IMAGE in $(pwd)
     docker build -t $IMAGE .
