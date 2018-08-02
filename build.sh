23f9e30da50841377e830ca2b455bed4cffd91c1
jiang <jiang.guo@pegasus.ae>
2018-08-02 13:17:46 +0800

    init

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ TAG=2.3.1-hadoop2.7
 
 build() {
     NAME=$1
-    IMAGE=bde2020/spark-$NAME:$TAG
+    IMAGE=jerdbo/spark-$NAME:$TAG
     cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
     echo '--------------------------' building $IMAGE in $(pwd)
     docker build -t $IMAGE .
@@ -16,7 +16,7 @@ build() {
 build base
 build master
 build worker
-build submit
-build java-template template/java
+#build submit
+#build java-template template/java
 #build python-template template/scala
-build python-template template/python
+#build python-template template/python
